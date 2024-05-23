607c775fc2da76976494237eb040fbc4c004b602
michellemwangi01 <michellemwangim@gmail.com>
2024-05-24 00:13:10 +0300

    display data in reverse

--- src/app/Components/TasksList.js
+++ src/app/Components/TasksList.js
@@ -49,42 +49,45 @@ const TasksList = () => {
           </tr>
         </thead>
         <tbody className="bg-white divide-y divide-gray-200 text-center ">
-          {taskListItems.map((item, index) => (
-            <tr key={index}>
-              <td className="px-6 py-4 whitespace-nowrap text-center">{`00${
-                index + 1
-              }`}</td>
-              <td className="px-6 py-4 whitespace-nowrap">
-                {item.client_name}
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap">
-                {item.client_email}
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap">
-                {item.client_phone}
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap">
-                {item.company_name}
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap">
-                {item.client_location}
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap">
-                {item.preferred_contact_method}
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap">{item.category}</td>
-              <td className="px-6 py-4 whitespace-nowrap">
-                {item.platform_service_product}
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap">{item.severity}</td>
-              <td className="px-6 py-4 whitespace-nowrap">
-                {item.issue_description}
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap text-green-800">
-                <MdDelete />
-              </td>
-            </tr>
-          ))}
+          {taskListItems
+            .slice()
+            .reverse()
+            .map((item, index) => (
+              <tr key={index}>
+                <td className="px-6 py-4 whitespace-nowrap text-center">{`00${
+                  index + 1
+                }`}</td>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  {item.client_name}
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  {item.client_email}
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  {item.client_phone}
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  {item.company_name}
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  {item.client_location}
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  {item.preferred_contact_method}
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap">{item.category}</td>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  {item.platform_service_product}
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap">{item.severity}</td>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  {item.issue_description}
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap text-green-800">
+                  <MdDelete />
+                </td>
+              </tr>
+            ))}
         </tbody>
       </table>
     </div>
